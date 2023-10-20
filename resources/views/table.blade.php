c28dfe82b63bf42ac459f65d8eb4dc9394ce5094
muhammaddwihariyanto <66157483+muhammaddwihariyanto@users.noreply.github.com>
2023-10-20 08:59:18 +0700

    ok

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -21,8 +21,7 @@
 
           <div class="card">
             <div class="card-body">
-              <h5 class="card-title">Datatables</h5>
-              <p>Add lightweight datatables to your project with using the <a href="https://github.com/fiduswriter/Simple-DataTables" target="_blank">Simple DataTables</a> library. Just add <code>.datatable</code> class name to any table you wish to conver to a datatable</p>
+              <h5 class="card-title">Tabel Data Sensor</h5>
 
               <!-- Table with stripped rows -->
               <table class="table datatable">
@@ -31,7 +30,6 @@
                     <th scope="col">#</th>
                     <th scope="col">Sensor</th>
                     <th scope="col">Nilai</th>
-                    <th scope="col">Satuan</th>
                     <!-- <th scope="col" disabled>Start Date</th> -->
                   </tr>
                 </thead>
@@ -39,50 +37,43 @@
                   <tr>
                     <th scope="row">1</th>
                     <td>Nitrogen</td>
-                    <td>Designer</td>
-                    <td>28</td>
+                    <td>7</td>
                     <!-- <td>2016-05-25</td> -->
                   </tr>
                   <tr>
                     <th scope="row">2</th>
                     <td>Fosfor</td>
-                    <td>Developer</td>
-                    <td>35</td>
+                    <td>9</td>
                     <!-- <td>2014-12-05</td> -->
                   </tr>
                   <tr>
                     <th scope="row">3</th>
                     <td>Kalium</td>
-                    <td>Finance</td>
-                    <td>45</td>
+                    <td>5</td>
                     <!-- <td>2011-08-12</td> -->
                   </tr>
                   <tr>
                     <th scope="row">4</th>
                     <td>Ph</td>
-                    <td>HR</td>
-                    <td>34</td>
+                    <td>6.85</td>
                     <!-- <td>2012-06-11</td> -->
                   </tr>
                   <tr>
                     <th scope="row">5</th>
                     <td>Kelembaban</td>
-                    <td>Dynamic Division Officer</td>
                     <td>47</td>
                     <!-- <td>2011-04-19</td> -->
                   </tr>
                   <tr>
                     <th scope="row">5</th>
                     <td>konduktivitas</td>
-                    <td>Dynamic Division Officer</td>
-                    <td>47</td>
+                    <td>18</td>
                     <!-- <td>2011-04-19</td> -->
                   </tr>
                   <tr>
                     <th scope="row">5</th>
                     <td>Tempratur</td>
-                    <td>Dynamic Division Officer</td>
-                    <td>47</td>
+                    <td>30</td>
                     <!-- <td>2011-04-19</td> -->
                   </tr>
                 </tbody>
